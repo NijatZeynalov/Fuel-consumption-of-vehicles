e28b6515fc2d4d62aed1dfee8cd073b2adab6493
Nijat Zeynalov <31247506+NijatZeynalov@users.noreply.github.com>
2020-07-21 21:48:25 +0300

    Add files via upload

---
+++ explot_data_analysis.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy import stats
+from scipy.stats import norm, skew
+
+from sklearn.preprocessing import RobustScaler, StandardScaler
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import mean_squared_error
+from sklearn.base import clone
+
+
+# warning
+import warnings
+warnings.filterwarnings('ignore')
+
+column_name = ["MPG", "Cylinders", "Displacement","Horsepower","Weight","Acceleration","Model Year", "Origin"]
+data = pd.read_csv("auto-mpg.data", names = column_name, na_values = "?", comment = "\t",sep = " ", skipinitialspace = True)
+
+data = data.rename(columns = {"MPG":"target"})
+
+corr_matrix = data.corr()
+sns.clustermap(corr_matrix, annot = True, fmt = ".2f")
+plt.title("Correlation btw features")
+plt.show()
+
+threshold = 0.75
+filtre = np.abs(corr_matrix["target"])>threshold
+corr_features = corr_matrix.columns[filtre].tolist()
+sns.clustermap(data[corr_features].corr(), annot = True, fmt = ".2f")
+plt.title("Correlation btw features")
+plt.show()
+
+#multicollinearity
+sns.pairplot(data, diag_kind = "kde", markers = "+")
+plt.show()
+
+#cylinders and origin can be categorical (feature engineering)
+
+
+plt.figure()
+sns.countplot(data["Cylinders"])
+print(data["Cylinders"].value_counts())
+
+plt.figure()
+sns.countplot(data["Origin"])
+print(data["Origin"].value_counts())
+
+# box
+for c in data.columns:
+    plt.figure()
+    sns.boxplot(x = c, data = data, orient = "v")
